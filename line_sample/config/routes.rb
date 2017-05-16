bc1aaed31e8d22c0878937006535cf7d43de6cea
b4p31031 <b4p31031@shonan.bunkyo.ac.jp>
2017-05-16 23:20:45 +0900

    ユーザーとコメントの関連付け

--- line_sample/config/routes.rb
+++ line_sample/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'comment' 'comments#index'
+  root 'comments#index'
 
   namespace :api, format: 'json' do
     namespace :v1  do
