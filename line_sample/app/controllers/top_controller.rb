70008cc39577f305086b467f295e2c0a04468cdf
b4p31031 <b4p31031@shonan.bunkyo.ac.jp>
2017-05-22 10:51:59 +0900

    トーク履歴機能の作成

--- line_sample/app/controllers/top_controller.rb
+++ line_sample/app/controllers/top_controller.rb
@@ -4,4 +4,15 @@ class TopController < ApplicationController
   def index
     @users = User.where(:id.nin => [current_user&.id])
   end
+
+  def show
+    to_user_ids = has_tt_user_ids
+    @users = User.where(:id.in => to_user_ids)
+  end
+
+  private
+    def has_tt_user_ids
+      tt_ids = current_user.comment.pluck(:talk_thread_id).uniq
+      TalkThread.where(:id.in => tt_ids).pluck(:first_user_id, :second_user_id).flatten.uniq.delete_if {|user_id| user_id == current_user.id }
+    end
 end
--- line_sample/config/routes.rb
+++ line_sample/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
-
+  get  'talk_log' => 'top#show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "message" => 'comments#index'
